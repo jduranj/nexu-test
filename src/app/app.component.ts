8e770955dc399a418c8e2f04b79ff1f94dd92086
Jesús Durán <47229820+jduranj@users.noreply.github.com>
2020-05-22 15:52:51 -0500

    Add nexu front end test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'nexu';
+  marcas = [{'nombre': 'Test'}]
+  modelos = []
+  marcaSeleccionada = ''
+
+  agregarMarca(nombre) {
+    this.marcas.push({nombre});
+  }
+
+  agregarModelo(modelo, precio) {
+    this.modelos.push({modelo, precio, marcaSeleccionada: this.marcaSeleccionada})
+  }
+
+  cambiarMarca(value) {
+    this.marcaSeleccionada = value;
+  }
+
+  eliminarModelo(index){
+    this.modelos.splice(index, 1);
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatListModule} from '@angular/material/list';
+import {MatTableModule} from '@angular/material/table';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +20,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    NoopAnimationsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatListModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
